0328ee754cdfb9329399a760e258568253b1c2e4
mokshanirgutti <mokshasai910@gmail.com>
2024-06-02 12:53:03 +0530

    port

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,7 @@
 import { WebSocketServer } from 'ws';
 import { gameManager } from './gameManager';
-
-const wss = new WebSocketServer({ port: 8000 });
+const port : any = process.env.PORT || 8000;
+const wss = new WebSocketServer({ port });
 const gamemanager = new gameManager();
 
 wss.on('connection', function connection(ws) {
